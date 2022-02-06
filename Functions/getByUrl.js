6d51048d93b5ab09efbb8203df9a9c260f2046d4
venomVV <venomx2020x@gmail.com>
2022-02-06 18:17:02 +0100

    please work

--- Functions/getByUrl.js
+++ Functions/getByUrl.js
@@ -4,6 +4,9 @@ const cheerio = require("cheerio");
 const config = {
   headers: {
     Referer: "https://shahed4u.pro/",
+    "Cache-Control": "no-cache",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
   },
 };
 
